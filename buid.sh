961627a23e9a2fdc7eb90a02eb7a0a4d7fd56097
manyowastephano <manyowastephano@gmail.com>
2025-09-21 17:35:47 +0200

    sd

--- buid.sh
+++ buid.sh
@@ -5,7 +5,6 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 python manage.py makemigrations --noinput
-python manage.py migrate vottingapp --fake-initial
 python manage.py migrate --noinput
 # Collect static files
 
